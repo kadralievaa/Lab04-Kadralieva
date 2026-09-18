439f17ebf83259ccdfde897201135d1dc6ce4456
kadralievaa <adel.kadralieva@gmail.com>
2026-09-18 20:08:24 +0300

    Lab 04: Individual Assignment

--- Program.cs
+++ Program.cs
@@ -33,14 +33,65 @@
 // }
 
 //4
-int age = 16;
-double height = 1.4;
-bool hasAnAdult = true;
-if ((age >= 14 && height >= 1.5) || hasAnAdult)
-{
-    Console.WriteLine("Можно кататься"); 
-} else
-{
-    Console.WriteLine("Пока нельзя");
+// int age = 16;
+// double height = 1.4;
+// bool hasAnAdult = true;
+// if ((age >= 14 && height >= 1.5) || hasAnAdult)
+// {
+//     Console.WriteLine("Можно кататься"); 
+// } else
+// {
+//     Console.WriteLine("Пока нельзя");
+// }
+
+//A
+// Console.WriteLine();
+// Console.Write("Введите число:  ");
+// int num = int.Parse(Console.ReadLine());
+
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine($"Число {num} является четным");
+// }
+// else {
+//     Console.WriteLine($"Число {num} является нечетным");
+// }
+
+// Б
+// Console.WriteLine();
+// Console.Write("Введите оценку (2-5): ");
+// int grade = int.Parse(Console.ReadLine());
+
+// if (grade == 5) {
+//     Console.WriteLine("Отлично");
+// } else if (grade == 4) {
+//     Console.WriteLine("Хорошо");
+// } else if (grade == 3) {
+//     Console.WriteLine("Удовлетворительно");
+// } else if (grade == 2) {
+//     Console.WriteLine("Неудовлетворительно");
+// } else {
+//     Console.WriteLine("Неверная оценка");
+// }
+
+// Вариант 2
+// Console.WriteLine();
+// Console.WriteLine("Введите ваш возраст:  ");
+// int age = int.Parse(Console.ReadLine());
+
+// if (age >= 18) {
+//     Console.WriteLine("Доступ разрешен");
+// } else {
+//     Console.WriteLine("Доступ запрещен");
+// }
+
+// Вариант 8
+Console.WriteLine();
+Console.WriteLine("Введите год: ");
+int year = int.Parse(Console.ReadLine());
+
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    Console.WriteLine("Год високосный, в феврале 29 дней");
+} else {
+    Console.WriteLine("Год не високосный, в феврале 28 дней");
 }
-    
--- obj/Debug/net10.0/Lab04-Kadralieva.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab04-Kadralieva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab04-Kadralieva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a8901db57ae6cca76f5ec861b5164abf0b7bc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a8497ffad045140252a6bed959a46720f9ebb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab04-Kadralieva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab04-Kadralieva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
